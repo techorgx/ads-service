abe0000567621f3e6bb102e0bb49387574beacaa
Aman Rajput <v-amrajput@expediagroup.com>
2023-10-08 15:53:26 +0530

    added updateAdStatus method

--- grpc/src/main/kotlin/com/project/ads/grpc/AdsApi.kt
+++ grpc/src/main/kotlin/com/project/ads/grpc/AdsApi.kt
@@ -5,6 +5,8 @@ import com.project.ads.api.v1.CreateAdRequest
 import com.project.ads.api.v1.CreateAdResponse
 import com.project.ads.api.v1.GetAdRequest
 import com.project.ads.api.v1.GetAdResponse
+import com.project.ads.api.v1.UpdateAdStatusRequest
+import com.project.ads.api.v1.UpdateAdStatusResponse
 import com.project.ads.service.AdsService
 import org.lognet.springboot.grpc.GRpcService
 
@@ -18,5 +20,9 @@ class AdsApi(
 
     override suspend fun getAd(request: GetAdRequest): GetAdResponse {
         return adsService.getAd(request)
+    }
+
+    override suspend fun updateAdStatus(request: UpdateAdStatusRequest): UpdateAdStatusResponse {
+        return adsService.updateAdStatus(request)
     }
 }
--- grpc/src/main/kotlin/com/project/ads/service/AdsService.kt
+++ grpc/src/main/kotlin/com/project/ads/service/AdsService.kt
@@ -4,6 +4,8 @@ import com.project.ads.api.v1.CreateAdRequest
 import com.project.ads.api.v1.CreateAdResponse
 import com.project.ads.api.v1.GetAdRequest
 import com.project.ads.api.v1.GetAdResponse
+import com.project.ads.api.v1.UpdateAdStatusRequest
+import com.project.ads.api.v1.UpdateAdStatusResponse
 import org.springframework.stereotype.Service
 
 @Service
@@ -14,5 +16,9 @@ class AdsService {
 
     fun getAd(request: GetAdRequest): GetAdResponse {
         return GetAdResponse.getDefaultInstance()
+    }
+
+    fun updateAdStatus(request: UpdateAdStatusRequest): UpdateAdStatusResponse {
+        return UpdateAdStatusResponse.getDefaultInstance()
     }
 }
